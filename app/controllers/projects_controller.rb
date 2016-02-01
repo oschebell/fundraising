e7b4aff27611c48396852ecaf1abf881aee7af33
oschebell <oschebell@gmail.com>
2016-02-01 18:18:52 +1100

    Added and passed initial feature specs

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,7 @@
+class ProjectsController < ApplicationController
+
+def index
+  @projects = ['Project 1', 'Project 2', 'Project 3']
+end
+
+end
--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -2,5 +2,13 @@ require "spec_helper"
 
 
 describe "Viewing the list of projects" do
-  it "shows the project"
+  it "shows three project names"  do
+    visit projects_url
+
+    expect(page).to have_text("3 Projects")
+
+    expect(page).to have_text("Project 1")
+    expect(page).to have_text("Project 2")
+    expect(page).to have_text("Project 3")
+  end
 end
